ded77c1748eb714524cd3e79880dfe6f204208f9
Benedikt Deicke <benedikt@benediktdeicke.com>
2021-09-17 11:15:28 +0200

    Don't mock addEventListener anymore

--- packages/widget/test/helper.js
+++ packages/widget/test/helper.js
@@ -3,8 +3,4 @@ var WorkerThreads = require('worker_threads');
 if(global.MessageChannel === undefined) {
   global.MessageChannel = WorkerThreads.MessageChannel;
   global.MessagePort = WorkerThreads.MessagePort;
-
-  global.MessagePort.prototype.addEventListener = function(name, callback) {
-    this.on('message', (data) => { callback({ data }) });
-  }
 }
