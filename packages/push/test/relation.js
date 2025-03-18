8b9690976fe94123f353336bcb0a04e00fa72b1b
Leo Euclides <leoeuclidss@gmail.com>
2025-03-18 16:43:21 -0300

    Adds push and update to relation

--- packages/push/test/relation.js
+++ packages/push/test/relation.js
@@ -1,52 +1,106 @@
-import { expect, use } from 'chai';
-import { createStubInstance } from 'sinon';
-import sinonChai from 'sinon-chai';
+import { expect, use } from "chai";
+import { createStubInstance } from "sinon";
+import sinonChai from "sinon-chai";
 
 use(sinonChai);
 
-import Relation from '../src/relation';
-import User from '../src/user';
-import Client from '../src/client';
+import Relation from "../src/relation";
+import User from "../src/user";
+import Client from "../src/client";
 
-describe('Relation', function() {
+describe("Relation", function () {
   let resource = User;
   let scope = {};
 
   let relation;
   let client;
 
-  beforeEach(function() {
+  beforeEach(function () {
     client = createStubInstance(Client);
     scope.client = client;
 
     relation = new Relation(scope, resource);
   });
 
-  it('should throw the resource\'s validation errors', function() {
-    expect(function() {
+  it("should throw the resource's validation errors", function () {
+    expect(function () {
       relation.create();
-    }).to.throw('Missing required attributes object')
+    }).to.throw("Missing required attributes object");
   });
 
-  describe('#create', function() {
+  describe("#create", function () {
     let client = createStubInstance(Client);
 
-    beforeEach(function() {
+    beforeEach(function () {
       scope.client = client;
     });
 
-    it('should post the resource using the scope\'s client', function () {
-      relation.create({ identifier: '1' });
+    it("should post the resource using the scope's client", function () {
+      relation.create({ identifier: "1" });
 
-      expect(client.post).to.be.calledWithExactly('/users', new User({ identifier: '1' }));
+      expect(client.post).to.be.calledWithExactly(
+        "/users",
+        new User({ identifier: "1" })
+      );
     });
   });
 
-  describe('#delete', function() {
-    it('should delete the resource using the scope\'s client', function () {
-      relation.delete('1');
+  describe("#push", function () {
+    let client = createStubInstance(Client);
 
-      expect(client.delete).to.be.calledWithExactly('/users/1');
+    beforeEach(function () {
+      scope.client = client;
+    });
+
+    it("should post the resource using the scope's client", function () {
+      relation.push({ identifier: "1" });
+
+      expect(client.post).to.be.calledWithExactly(
+        "/users",
+        new User({ identifier: "1" })
+      );
+    });
+  });
+
+  describe("#create", function () {
+    let client = createStubInstance(Client);
+
+    beforeEach(function () {
+      scope.client = client;
+    });
+
+    it("should post the resource using the scope's client", function () {
+      relation.create({ identifier: "1" });
+
+      expect(client.post).to.be.calledWithExactly(
+        "/users",
+        new User({ identifier: "1" })
+      );
+    });
+  });
+
+  describe("#update", function () {
+    let client = createStubInstance(Client);
+
+    beforeEach(function () {
+      scope.client = client;
+    });
+
+    it("should post the resource using the scope's client", function () {
+      relation.update({ identifier: "1" });
+
+      expect(client.post).to.be.calledWithExactly(
+        "/users",
+        new User({ identifier: "1" })
+      );
+    });
+  });
+
+  describe("#delete", function () {
+    it("should delete the resource using the scope's client", function () {
+      relation.delete("1");
+
+      expect(client.delete).to.be.calledWithExactly("/users/1");
     });
   });
 });
